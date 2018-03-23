b96b34f3aa420de81c72597d86680ab84231766a
Vedant Puri <vedantpuri13@gmail.com>
2018-03-23 00:43:45 -0400

    Add Flag model

--- CS326TeamH/ffs/admin.py
+++ CS326TeamH/ffs/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Product
 from .models import User
+from .models import Flag
 
 admin.site.register(Product)
 admin.site.register(User)
+admin.site.register(Flag)
--- CS326TeamH/ffs/models.py
+++ CS326TeamH/ffs/models.py
@@ -8,6 +8,11 @@ class Product(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=7, default=39.99)
     image = models.FileField(upload_to='products/', null=True, blank=True)
     owner = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    Availability = (
+        ('a', 'Available'),
+        ('s', 'Sold'),
+    )
+    status = models.CharField(max_length=1, choices=Availability, blank=True, default='a', help_text='Item availability')
 
     def __str__(self):
         return self.title
@@ -16,10 +21,26 @@ class User(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     bio = models.TextField()
-    college_name = models.CharField(max_length=100)
+    password = models.CharField(max_length=120, blank=True, help_text="Enter a password")
+    College = (
+        ('u', 'Umass Amherst'),
+        ('a', 'Amherst College'),
+        ('s', 'Smith College'),
+        ('m', 'Mount Holyoke College'),
+        ('h', 'Hamshire College'),
+    )
+
+    college = models.CharField(max_length=1, choices=College, blank=True, default='u', help_text='college choice')
     email = models.EmailField(max_length=100)
     star_count = models.DecimalField(decimal_places=0, max_digits=1)
     image = models.FileField(upload_to='user_profile_picture/', null=True, blank=True)
 
     def __str__(self):
         return '{0}, {1}'.format(self.first_name, self.last_name)
+
+class Flag(models.Model):
+    user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True, blank=True)
+    products = models.ManyToManyField('Product', blank=True)
+
+    def __str__(self):
+        return str(self.id)
