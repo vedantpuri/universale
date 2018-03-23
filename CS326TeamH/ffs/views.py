a774fc558132de2273361de6455afa138c57ee8f
Shubham Mehta <smlakers@gmail.com>
2018-03-23 18:49:01 -0400

    Working /product URL mapping

--- CS326TeamH/ffs/views.py
+++ CS326TeamH/ffs/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
+from ffs.models import Product
 # Create your views here.
 def product_list_view(request):
-	queryset = Product.objects().filter(title__icontains='textbook')
+	queryset = Product.objects.filter(title__icontains='textbook')
 	
 	context = { 
 		'object_list':queryset
