88e9a9adfc685a5f5d26f7c556beb8b1eed7a1fd
Shubham Mehta <smlakers@gmail.com>
2018-03-23 19:23:09 -0400

    Image rendering working properly

--- CS326TeamH/CS326TeamH/urls.py
+++ CS326TeamH/CS326TeamH/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +28,4 @@ urlpatterns = [
 
 
 # Not sure if this is needed
-#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
