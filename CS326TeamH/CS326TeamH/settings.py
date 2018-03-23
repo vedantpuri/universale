eff52695d6baa6ab873c98323bd851f335dad936
Vedant Puri <vedantpuri13@gmail.com>
2018-03-22 22:45:20 -0400

    Add product model

--- CS326TeamH/CS326TeamH/settings.py
+++ CS326TeamH/CS326TeamH/settings.py
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
--- CS326TeamH/ffs/models.py
+++ CS326TeamH/ffs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
+    image = models.FileField(upload_to='products/', null=True, blank=True)
+    # owner = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
