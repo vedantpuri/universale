3657ad6eff8d8931aa5257a16cce57fc254aa353
Vedant Puri <vedantpuri13@gmail.com>
2018-03-26 15:42:53 -0400

    Render Name, profile, email correctly

--- CS326TeamH/ffs/views.py
+++ CS326TeamH/ffs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from ffs.models import Product
+from ffs.models import User
 # Create your views here.
 def product_list_view(request):
 	queryset = Product.objects.filter(title__icontains='textbook')
@@ -11,7 +12,19 @@ def product_list_view(request):
 	return render(request, 'details.html', context)
 
 def user_view(request):
+	user = User.objects.get(email='samuelljackson@umass.edu')
+	fname = user.first_name
+	lname = user.last_name
+	# college = user.get_College()
+	bio = user.bio
+	image = user.image
+	email = user.email
 	context = {
-		'name': "Vedant"
+		'fname': fname,
+		'lname': lname,
+		# 'college': college,
+		# 'bio': bio
+		'image' : image,
+		'email' : email
 	}
 	return render(request, 'user.html', context)
