31702a9c4dca9d7dcdbf88d03c3ac4159897bdaf
Ivanz7 <yifanzhu@umass.edu>
2018-04-08 18:47:21 -0400

    fix error

--- CS326TeamH/ffs/models.py
+++ CS326TeamH/ffs/models.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=7, default=39.99)
-    image = models.FileField(upload_to='products/', null=True, blank=True)
+    image = models.FileField(upload_to='products/', null=True)
     owner = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
     Availability = (
         ('a', 'Available'),
