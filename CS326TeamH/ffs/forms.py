35695ed95ac844f5f0685d5300c7eb36e2fe5993
Ivanz7 <yifanzhu@umass.edu>
2018-04-08 18:54:12 -0400

    change title min length to 5

--- CS326TeamH/ffs/forms.py
+++ CS326TeamH/ffs/forms.py
@@ -13,14 +13,10 @@ from ffs.models import Product
 from django.core.exceptions import ValidationError
 
 class UploadProductForm(ModelForm):
-   title = forms.CharField(label='Item Name', max_length=100, required=True)
+   title = forms.CharField(label='Item Name', min_length=5, max_length=100, required=True)
    description = forms.CharField(widget=forms.Textarea, min_length=10)
    price = forms.IntegerField(min_value=0, required=True)
-<<<<<<< HEAD
-   image = forms.ImageField(required=True)
-=======
-   # item_image = forms.FileField(required=True)
->>>>>>> 7a5481f86dbca9e2725db35964819fa7f17b0aa2
+   # image = forms.ImageField(required=True)
    class Meta:
        model = Product
        fields = ['title', 'description', 'price', 'image']
