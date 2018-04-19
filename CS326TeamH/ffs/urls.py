47204559d311e18238bd6a0a797263767aa5a40c
Sagar Thapar <sthapar@umass.edu>
2018-04-19 16:56:11 -0400

    Add flag and Unflag implementation

--- CS326TeamH/ffs/urls.py
+++ CS326TeamH/ffs/urls.py
@@ -8,12 +8,14 @@ from ffs.views import landing_view
 from ffs.views import upload_view
 from ffs.views import edit_profile_view
 from ffs.views import add_to_flagged
+from ffs.views import remove_from_flagged
 
 urlpatterns = [
 	path('user/', user_view, name='user'),
 	path('search/', search_view, name='search'),
 	path('flagged/', flagged_view, name='flag'),
-	path('user/add/', add_to_flagged, name='flag_add'),
+	path('flagged/remove/', remove_from_flagged, name='flag_remove'),
+	path('search/add/', add_to_flagged, name='search_add'),
 	path('upload/', upload_view, name='upload'),
 	path('edit_profile/', edit_profile_view, name="user_edit"),
 	path('', landing_view, name='home')
