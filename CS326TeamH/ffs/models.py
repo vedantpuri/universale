c723669e84d2d03224b1d5ae3a4eced9e683d3c3
Vedant Puri <vedantpuri13@gmail.com>
2018-04-25 10:50:51 -0400

    Fix models.py

--- CS326TeamH/ffs/models.py
+++ CS326TeamH/ffs/models.py
@@ -42,15 +42,6 @@ class User(models.Model):
     def __str__(self):
         return '{0}, {1}'.format(self.first_name, self.last_name)
 
-<<<<<<< HEAD
-=======
-# @receiver(post_save, sender=User)
-# def update_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
-#     instance.profile.save()
-
->>>>>>> 2d8ff48... Signup with custom user model served at localhost:8000/signup; issue with image
 class Flag(models.Model):
     user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True, blank=True)
     products = models.ManyToManyField('Product', blank=True)
