2db870183a02cbc020f39649489d97bbd2f11517
Mohsin Mumtaz <mohsinmumtaz21@gmail.com>
2018-07-29 12:25:13 +0530

    Cleanup models file.
    Signed-off-by: Mohsin Mumtaz <mohsinmumtaz21@gmail.com>

--- Django/ffs/models.py
+++ Django/ffs/models.py
@@ -1,21 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from ffs.choices import *
+
+
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=7, default=39.99)
     image = models.FileField(upload_to='products/', null=True)
     owner = models.ForeignKey('Student', on_delete=models.SET_NULL, null=True)
-    Availability = (
-        ('a', 'Available'),
-        ('s', 'Sold'),
-    )
-    status = models.CharField(max_length=1, choices=Availability, blank=True, default='a', help_text='Item availability')
+
+    status = models.CharField(max_length=1, choices=Availability,
+                              blank=True, default='a', help_text='Item availability')
     flag_count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
+
 
 class Student(models.Model):
     django_user = models.OneToOneField(User, unique=True, null=True, on_delete=models.CASCADE)
@@ -23,21 +25,15 @@ class Student(models.Model):
     last_name = models.CharField(max_length=100)
     bio = models.TextField()
     password = models.CharField(max_length=120, blank=True, help_text="Enter a password")
-    College = (
-        ('u', 'Umass Amherst'),
-        ('a', 'Amherst College'),
-        ('s', 'Smith College'),
-        ('m', 'Mount Holyoke College'),
-        ('h', 'Hampshire College'),
-    )
-
-    college = models.CharField(max_length=1, choices=College, blank=True, default='u', help_text='college choice')
+    college = models.CharField(max_length=1, choices=Colleges,
+                               blank=True, default='u', help_text='college choice')
     email = models.EmailField(max_length=100)
     star_count = models.DecimalField(decimal_places=0, max_digits=1)
     image = models.FileField(upload_to='user_profile_picture/', null=True, blank=True)
 
     def __str__(self):
         return '{0}, {1}'.format(self.first_name, self.last_name)
+
 
 class Flag(models.Model):
     user = models.ForeignKey('Student', on_delete=models.SET_NULL, null=True, blank=True)
